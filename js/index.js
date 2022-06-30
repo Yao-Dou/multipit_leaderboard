7d162b26fe1f47961f52eaf9e5efc78042de4738
Yao Dou <douy@cs.washington.edu>
2022-06-30 16:54:13 -0400

    add current data

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 var filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/leaderboard_test.csv";
+var json_filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/leaderboard_test.json";
 
 $(document).ready(function() {
     // read the csv file in data folder
@@ -20,48 +21,57 @@ $(document).ready(function() {
     // }
     // });
 
-    $('#crowd_leaderboard').DataTable({
-        ajax: '../data/leaderboard_test.txt',
-        // columns: [
-        //     {
-        //         data: 'model',
-        //     },
-        //     {
-        //         data: 'F1',
-        //     },
-        //     {
-        //         data: 'Precision',
-        //     },
-        //     {
-        //         data: 'Recall',
-        //     },
-        //     {
-        //         data: 'Accuracy',
-        //     },
-        //     {
-        //         data: 'F11',
-        //     },
-        //     {
-        //         data: 'Precision1',
-        //     },
-        //     {
-        //         data: 'Recall1',
-        //     },
-        //     {
-        //         data: 'Accuracy1',
-        //     },
-        //     {
-        //         data: 'F12',
-        //     },
-        //     {
-        //         data: 'Precision2',
-        //     },
-        //     {
-        //         data: 'Recall2',
-        //     },
-        //     {
-        //         data: 'Accuracy2',
-        //     },
-        // ],
+    var table = $('#crowd_leaderboard').DataTable({
+        ajax: json_filepath,
+        columns: [
+            {
+                data: 'model',
+            },
+            {
+                data: 'F1',
+            },
+            {
+                data: 'Precision',
+            },
+            {
+                data: 'Recall',
+            },
+            {
+                data: 'Accuracy',
+            },
+            {
+                data: 'F11',
+            },
+            {
+                data: 'Precision1',
+            },
+            {
+                data: 'Recall1',
+            },
+            {
+                data: 'Accuracy1',
+            },
+        ],
+        columnDefs: [{
+            "defaultContent": "-",
+            "targets": "_all"
+          }],
+        lengthMenu: [
+            [10, 20, -1],
+            [10, 20, 'All'],
+        ],
+        // scrollX: true,
+        scrollY: '50vh',
+        scrollCollapse: true,
+        paging: false,
+        searching: true,
+        info: false,
+        dom: 't',
     });
+
+    $('#search-box').keyup(function(){
+        table.search($(this).val()).draw() ;
+    })
+
+    // get the position of the
 });
