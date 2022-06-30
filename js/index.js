565891a3ee34ec78b77463568cbe9dd1ec6bf16c
Yao Dou <douy@cs.washington.edu>
2022-06-30 17:53:52 -0400

    update metrics

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 
-var filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/leaderboard_test.csv";
-var json_filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/leaderboard_test.json";
+// var filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/leaderboard_test.csv";
+var loose_json_filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/loose_identification_performance.json";
+var strict_json_filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/strict_identification_performance.json";
+var gen_json_filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/generation_performance.json";
+var gen_metric_json_filepath = "https://raw.githubusercontent.com/Yao-Dou/multipit_leaderboard/master/data/generation_metrics.json";
 
 $(document).ready(function() {
     // read the csv file in data folder
@@ -21,14 +24,24 @@ $(document).ready(function() {
     // }
     // });
 
-    var table = $('#crowd_leaderboard').DataTable({
-        ajax: json_filepath,
+    var table = $('#loose_iden_leaderboard').DataTable({
+        ajax: loose_json_filepath,
         columns: [
             {
-                data: 'model',
+                data:null,
+                render: function(data, type, row, meta) {
+                    return meta["row"] + 1;
+                },
+                orderable: false,
+                searchable: false,
+                targets: 0,
             },
             {
-                data: 'F1',
+                data: 'Model',
+                orderable: false
+            },
+            {
+                data: 'Date',
             },
             {
                 data: 'Precision',
@@ -40,16 +53,7 @@ $(document).ready(function() {
                 data: 'Accuracy',
             },
             {
-                data: 'F11',
-            },
-            {
-                data: 'Precision1',
-            },
-            {
-                data: 'Recall1',
-            },
-            {
-                data: 'Accuracy1',
+                data: 'F1',
             },
         ],
         columnDefs: [{
@@ -67,6 +71,157 @@ $(document).ready(function() {
         searching: true,
         info: false,
         dom: 't',
+        order: [[6, 'dsc']]
+    });
+
+    $('#strict_iden_leaderboard').DataTable({
+        ajax: strict_json_filepath,
+        columns: [
+            {
+                data:null,
+                render: function(data, type, row, meta) {
+                    return meta["row"] + 1;
+                },
+                orderable: false,
+                searchable: false,
+                targets: 0,
+            },
+            {
+                data: 'Model',
+                orderable: false
+            },
+            {
+                data: 'Date',
+            },
+            {
+                data: 'Precision',
+            },
+            {
+                data: 'Recall',
+            },
+            {
+                data: 'Accuracy',
+            },
+            {
+                data: 'F1',
+            },
+        ],
+        columnDefs: [{
+            "defaultContent": "-",
+            "targets": "_all"
+          }],
+        lengthMenu: [
+            [10, 20, -1],
+            [10, 20, 'All'],
+        ],
+        // scrollX: true,
+        scrollY: '50vh',
+        scrollCollapse: true,
+        paging: false,
+        searching: true,
+        info: false,
+        dom: 't',
+        order: [[6, 'dsc']]
+    });
+
+    $('#gen_leaderboard').DataTable({
+        ajax: gen_json_filepath,
+        columns: [
+            {
+                data:null,
+                render: function(data, type, row, meta) {
+                    return meta["row"] + 1;
+                },
+                orderable: false,
+                searchable: false,
+                targets: 0,
+            },
+            {
+                data: 'Model',
+                orderable: false
+            },
+            {
+                data: 'Date',
+            },
+            {
+                data: 'BERT-iBLEU',
+            },
+            {
+                data: 'Self-BLEU',
+            },
+            {
+                data: 'BERT-Score',
+            },
+            {
+                data: 'BLEU',
+            },
+        ],
+        columnDefs: [{
+            "defaultContent": "-",
+            "targets": "_all"
+          }],
+        lengthMenu: [
+            [10, 20, -1],
+            [10, 20, 'All'],
+        ],
+        // scrollX: true,
+        scrollY: '50vh',
+        scrollCollapse: true,
+        paging: false,
+        searching: true,
+        info: false,
+        dom: 't',
+        order: [[3, 'dsc']]
+    });
+
+    $('#gen_metrics').DataTable({
+        ajax: gen_metric_json_filepath,
+        columns: [
+            {
+                data:null,
+                render: function(data, type, row, meta) {
+                    return meta["row"] + 1;
+                },
+                orderable: false,
+                searchable: false,
+                targets: 0,
+            },
+            {
+                data: 'Metric',
+                orderable: false
+            },
+            {
+                data: 'Referenceless',
+            },
+            {
+                data: 'Fluency Correlation',
+            },
+            {
+                data: 'Semantic Similarity Correlation',
+            },
+            {
+                data: 'Diversity Correlation',
+            },
+            {
+                data: 'Overall Correlation',
+            },
+        ],
+        columnDefs: [{
+            "defaultContent": "-",
+            "targets": "_all"
+          }],
+        lengthMenu: [
+            [10, 20, -1],
+            [10, 20, 'All'],
+        ],
+        // scrollX: true,
+        scrollY: '50vh',
+        scrollCollapse: true,
+        paging: false,
+        searching: true,
+        info: false,
+        dom: 't',
+        order: [[5, 'dsc']]
     });
 
     $('#search-box').keyup(function(){
